61355c9c8bf940219597cac641546e3852b257bc
Unknown <munkyung0324@gmail.com>
2018-12-30 19:19:20 +0900

    InputInfo 화면작성
    InputInfoActivity.java : 월급실수령액, 입사일, 급여일 정보 받는 화면작성
    
    - 일러스트 이미지 조정 필요
    - 글꼴으로 진행할 지 글꼴 이미지로 진행할지 보류

---
+++ app/src/main/java/dpm/project/b/b_project/input/InputInfoActivity.java
@@ -0,0 +1,15 @@
+package dpm.project.b.b_project.input;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dpm.project.b.b_project.R;
+
+public class InputInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_info);
+    }
+}
